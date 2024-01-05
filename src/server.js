0abb5e50417b82aeec1829ca859bf86a2d57a01f
Alex <baileymk@hotmail.com>
2024-01-05 12:08:51 +0000

    amqp credentials as env vars

--- src/server.js
+++ src/server.js
@@ -23,7 +23,8 @@ const connectRabbitMQ = async() => {
     try {
 
         // connection = await amqp.connect("amqp://default_user_h19T9cxik7_FXOICj2Y:r_h_Tv11_oVQ2pts5BE0lMncX9RxfY68@10.97.41.66");
-        connection = await amqp.connect("amqp://default_user_zr37xV6wIH_rXbkMbP1:W1i3xQ8q7dTZMK0fvyZyJpfW7Pw8Q809@10.102.238.168");
+        // connection = await amqp.connect("amqp://default_user_zr37xV6wIH_rXbkMbP1:W1i3xQ8q7dTZMK0fvyZyJpfW7Pw8Q809@10.102.238.168");
+        connection = await amqp.connect(`amqp://${process.env.SECRET_USERNAME}:${process.env.SECRET_PASSWORD}@10.102.238.168`);
 
         channel = await connection.createChannel();
         await channel.assertQueue("update_profile", { durable: false });
